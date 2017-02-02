90b8d62149262ff1636b83ccbec9563ecf4e3ed9
sniper24 <brianxie89@gmail.com>
2017-02-03 00:04:30 +0800

    Init
    Init

---
+++ 00_MoreClasses.py
@@ -0,0 +1,6 @@
+#coding:utf-8
+QValidator
+QRegExpValidator
+
+
+QTabBar 选项卡
---
+++ 03_quitbutton.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+# quitbutton.py
+
+import sys
+from PyQt4 import QtGui, QtCore
+
+print help(QtCore.SIGNAL)
